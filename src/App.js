4d2e81382aafbb528ebe9d64578dadebd86ef78e
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-20 13:47:32 +0100

    added title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Title />
+      </main>
       <p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
