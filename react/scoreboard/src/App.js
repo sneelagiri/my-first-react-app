21ea5e2ec91867ede43fdc5d6ebbde96f5aa2880
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-20 12:21:54 +0100

    deploying my first react app using Netlify

---
+++ react/scoreboard/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello World!
+        </a>
+        <br></br>
+        I feel very positive about JavaScript and React. I think that they can be used efficiently to solve real-world problems, which businesses across the world face. 
+      </p>
+    </div>
+  );
+}
+
+export default App;
